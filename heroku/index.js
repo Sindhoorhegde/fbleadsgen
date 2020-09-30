a9f7fa6dee610856971fb5135d245ba04f46f7f4
Sindhoorhegde <72134132+Sindhoorhegde@users.noreply.github.com>
2020-09-30 17:29:32 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -21,7 +21,8 @@ var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
-  console.log(req);
+    console.log(req);
+    console.log(JSON.stringify(received_updates, null, 2));
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
 
