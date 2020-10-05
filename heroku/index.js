9726ab3fe767ffd5c6f7dfadb99d75d99ebd67ac
Sindhoorhegde <72134132+Sindhoorhegde@users.noreply.github.com>
2020-10-05 19:09:17 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -38,7 +38,7 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 });
 
 app.post('/facebook', function (req, res) {
-    console.log('Facebook request:', req);
+    console.log('Facebook request:', req.headers);
   //console.log('Facebook request body:', req.body);
   
   if (!req.isXHubValid()) {
